22a2cd72f9efd6d5e6c5fef222ea88312b1775b9
Zeek <mzulkiful@yahoo.com>
2022-09-24 06:59:43 +0100

    Search Results Error Message added

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,6 +4,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function RecipeList({recipes}) {
+
+        if (recipes.length === 0) {
+            return <div className='error'>No recipes to load...</div>
+        }
+
   return (
     <div className='recipe-list'>
       {recipes.map(recipe => (
